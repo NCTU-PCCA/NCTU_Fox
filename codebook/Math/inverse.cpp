922a2853cf4c1406cd60b92347b4a9e68c8cbe31
cthbst <edison1998402@gmail.com>
2018-03-17 18:08:32 +0800

    add inverse.cpp

---
+++ codebook/Math/inverse.cpp
@@ -0,0 +1,13 @@
+const int MAXN = 1000006;
+int inv[MAXN]; 
+void invTable(int bound, int p){
+    inv[1] = 1;
+    for (int i=2; i<bound; i++){
+        inv[i] = (long long)inv[i] * (p-p/i) %p;
+    }
+}
+
+int inv(int b, int p){
+    if (b==1) return 1;
+    return (long long)inv(p%b,p) * (p-p/b) %p;
+}
