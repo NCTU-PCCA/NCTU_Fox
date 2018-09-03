cc36cc5ffd032d9b0125b6c0d962310640902a0e
cthbst <edison1998402@gmail.com>
2018-09-03 22:14:31 +0800

    python add decimal

--- codebook/Other/python.py
+++ codebook/Other/python.py
@@ -72,3 +72,20 @@ choice(list) # pick 1 item from list
 choices(list,k) # pick k item
 shuffle(list)
 Uniform(L,R) # float from [L,R]
+
+# Decimal
+from fractions import Fraction
+from decimal import Decimal, getcontext
+getcontext().prec = 250 # set precision
+
+itwo = Decimal(0.5)
+two = Decimal(2)
+
+N = 200
+def angle(cosT):
+  """given cos(theta) in decimal return theta"""
+  for i in range(N):
+    cosT = ((cosT + 1) / two) ** itwo
+  sinT = (1 - cosT * cosT) ** itwo
+  return sinT * (2 ** N)
+pi = angle(Decimal(-1))
