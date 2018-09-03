9ebc212a6963d4738190c501e54fb08c26222f65
ss1h2a3tw <ss1ha3tw@gmail.com>
2018-09-03 11:24:57 +0800

    Add increase stack size

--- codebook/Basic/T.cpp
+++ codebook/Basic/T.cpp
@@ -1,6 +1,14 @@
 #pragma GCC optimize("Ofast")
 #include <bits/stdc++.h>
+#include <sys/time.h>
+#include <sys/resource.h>
 using namespace std;
+
+void setstack(){
+    // Set soft limit and hard limit to max
+    const rlimit tmp {RLIM_INFINITY,RLIM_INFINITY};
+    setrlimit(RLIMIT_STACK,&tmp);
+}
 
 int main(){
     ios::sync_with_stdio(0);
