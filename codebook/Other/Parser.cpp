228aae156660796eda12bc80a524d29f4a80dc18
harryoooooooooo <ymsc27884@gmail.com>
2018-10-31 00:12:51 +0800

    upgrade parser

--- codebook/Other/Parser.cpp
+++ codebook/Other/Parser.cpp
@@ -1,103 +1,62 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-typedef long long T;
-bool GG;
-
-T Eval2(char *&end) {
-    T Eval0(char *&);
-    T res=0;
-    if ( *end=='(' ){
-        res = Eval0(++end);
-        if (*(end++)==')') return res;
-        else { GG = true; return -1; }
+using LL = long long;
+const int MAXLEVEL = 2;
+//  binary operators
+const vector<char> Ops[MAXLEVEL] = {
+    {'+', '-'}, // level 0
+    {'*', '/'}  // level 1
+};
+//  unary operators
+const vector<pair<char,int>> Op1s = {
+    {'-', 0} // operator negative works on level 0
+};
+//  throw when error occur !!!!!
+struct Parser{
+    struct Node{
+        ~Node(){ delete L; delete R; }
+        enum { op, op1, num } type;
+        LL val;
+        Node *L, *R;
+    } *root;
+    Parser(): root(nullptr){}
+    ~Parser(){ delete root; }
+    void build(istream& is) try{
+        delete root;
+        root = nullptr;
+        parse(0, root, is);
+        if ((is>>ws).peek() != EOF) throw 0;
+    } catch(...){ throw; }
+    void build(const string& s){
+        istringstream ss(s);
+        build(ss);
     }
-    else if( isdigit(*end) ){
-        return strtol(end, &end, 10);
-    }   // 可改成  {strtol ,strtoll strtod}
-    else { GG = true; return -1; }
-}
-
-T Evalx(char *&end){
-    if(GG) return -1;
-    T res = Eval2(end); if(GG) return -1;
-    while (*end == '%'){
-        end++;
-        res = ( res % Eval2(end) );
-        if(GG) return -1;
+    void parse(int LEVEL, Node*& x, istream& is){
+        if (LEVEL == MAXLEVEL){
+            is>>ws;
+            if (is.peek()>='0' && is.peek()<='9'){
+                LL t; is >>t;
+                x = new Node{Node::num, t, nullptr, nullptr};
+            } else if (is.peek() == '('){
+                is.get();
+                parse(0, x, is);
+                is>>ws;
+                if (is.get()!=')') throw 0;
+            } else throw 0;
+        } else {
+            is >>ws;
+            char op1 = 0;
+            for (auto& x : Op1s){
+                auto& op = x.first;
+                auto& lev = x.second;
+                if (LEVEL == lev && is.peek() == op)
+                    op1 = is.get();
+            }
+            parse(LEVEL+1, x, is);
+            if (op1) x = new Node{Node::op1, op1, x, nullptr};
+            auto& ops = Ops[LEVEL];
+            while (is>>ws && count(ops.begin(), ops.end(), is.peek())){
+                x = new Node{Node::op, is.get(), x, nullptr};
+                parse(LEVEL+1, x->R, is);
+            }
+        }
     }
-    return res;
-}
-
-T Eval1(char *&end) {
-    if(GG) return -1;
-    T res = Evalx(end); if(GG) return -1;
-    while (*end=='*' || *end == '/'){
-        end++;
-        if(*(end-1) == '*')res = ( res * Evalx(end) );
-        else if(*(end-1) == '/')res = ( res / Evalx(end) );
-        if(GG) return -1;
-    }
-    return res;
-}
-
-T Eval12(char *&end){
-    if(GG) return -1;
-    T res=1;
-    if(*end == '-'){
-        end++;
-        res = -1;
-    }
-    res *= Evalx(end);
-    while (*end=='*' || *end == '/'){
-        end++;
-        if(*(end-1) == '*')res = ( res * Evalx(end) );
-        else if(*(end-1) == '/')res = ( res / Evalx(end) );
-        if(GG) return -1;
-    }
-    return res;
-}
-T Eval0(char *&end) {
-    if(GG) return -1;
-    T res;
-    res = Eval12(end); if(GG) return -1;
-    while (*end=='+' || *end == '-'){
-        end++;
-        if(*(end-1) == '+')res = ( res + Eval1(end) );
-        else res = ( res - Eval1(end) );
-        if(GG) return -1;
-    }
-    return res;
-}
-
-T parse(char *s){
-    GG = false;
-    T res = Eval0(s);
-    while(*s != '\0'){
-        if(*s != ' ')GG = true;
-        s++;
-    }
-    return res;
-}
-
-int main() {
-    char expr[3003];
-    string str;
-    int cnt = 0;
-    while (getline (cin,str)){
-        printf("case %d:\n",++cnt);
-        strcpy(expr,str.c_str());
-        T ans = parse(expr);
-        if(GG) puts("syntactically incorrect\n");
-        else printf("%lld\n\n", ans);
-    }
-}
-
-/*
-E0 = E1' (+-E1)*
-E1 = Ex (/*Ex)*
-Ex = E2 (%E2)*
-E2 = (E0) or R+
-E1' = Ex (/* Ex)*  or  -Ex (/* Ex)*
-
-*/
+};
