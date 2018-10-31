5a7b9d2c7ef83496ec94b51c8d38b868bc0b7d5d
harryoooooooooo <ymsc27884@gmail.com>
2018-10-31 11:14:20 +0800

    better performance

--- codebook/Other/Parser.cpp
+++ codebook/Other/Parser.cpp
@@ -22,43 +22,44 @@ struct Parser{
     void build(istream& is) try{
         delete root;
         root = nullptr;
-        parse(0, root, is);
+        parse<0>(root, is);
         if ((is>>ws).peek() != EOF) throw 0;
     } catch(...){ throw; }
     void build(const string& s){
         istringstream ss(s);
         build(ss);
     }
-    void parse(int LEVEL, Node*& x, istream& is){
-        if (LEVEL == MAXLEVEL){
-            is>>ws;
-            if (is.peek()>='0' && is.peek()<='9'){
-                LL t; is >>t;
-                x = new Node{Node::num, t, nullptr, nullptr};
-            } else if (is.peek() == '('){
-                is.get();
-                parse(0, x, is);
-                is>>ws;
-                if (is.get()!=')') throw 0;
-            } else throw 0;
-        } else {
-            is >>ws;
-            char op1 = 0;
-            for (auto& x : Op1s){
-                auto& op = x.first;
-                auto& lev = x.second;
-                if (LEVEL == lev && is.peek() == op){
-                    op1 = is.get();
-                    break;
-                }
-            }
-            parse(LEVEL+1, x, is);
-            if (op1) x = new Node{Node::op1, op1, x, nullptr};
-            auto& ops = Ops[LEVEL];
-            while (is>>ws && count(ops.begin(), ops.end(), is.peek())){
-                x = new Node{Node::op, is.get(), x, nullptr};
-                parse(LEVEL+1, x->R, is);
-            }
+    char getOp1(int LEVEL, istream& is)const{
+        is >>ws;
+        for (auto& x : Op1s){
+            auto& op = x.first;
+            auto& lev = x.second;
+            if (LEVEL == lev && is.peek() == op)
+                return is.get();
+        }
+        return 0;
+    }
+    template <int LEVEL> void parse(Node*& x, istream& is){
+        char op1 = getOp1(LEVEL, is);
+        parse<LEVEL+1>(x, is);
+        if (op1) x = new Node{Node::op1, op1, x, nullptr};
+        auto& ops = Ops[LEVEL];
+        while (is>>ws && count(ops.begin(), ops.end(), is.peek())){
+            x = new Node{Node::op, is.get(), x, nullptr};
+            parse<LEVEL+1>(x->R, is);
         }
     }
 };
+template <> void Parser::parse<MAXLEVEL>(Node*& x, istream& is){
+    char op1 = getOp1(MAXLEVEL, is);
+    if (is.peek()>='0' && is.peek()<='9'){
+        LL t; is >>t;
+        x = new Node{Node::num, t, nullptr, nullptr};
+    } else if (is.peek() == '('){
+        is.get();
+        parse<0>(x, is);
+        is>>ws;
+        if (is.get()!=')') throw 0;
+    } else throw 0;
+    if (op1) x = new Node{Node::op1, op1, x, nullptr};
+}
