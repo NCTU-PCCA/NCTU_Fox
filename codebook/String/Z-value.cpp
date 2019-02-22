1f935da25253eaa46378e5b0728137675852471a
harryoooooooooo <ymsc27884@gmail.com>
2019-02-22 16:22:07 +0800

    Z-value: remove 'and'

--- codebook/String/Z-value.cpp
+++ codebook/String/Z-value.cpp
@@ -14,10 +14,10 @@ void Zpal(const char *s, int len, int *z) {
     // i is [i-z[i] .. i+z[i]]
     z[0] = 0;
     for (int b=0, i=1; i<len; i++) {
-        if (z[b] + b >= i) z[i] = min(z[2*b-i], b+z[b]-i);
+        if (z[b]+b >= i) z[i] = min(z[2*b-i], b+z[b]-i);
         else z[i] = 0;
-        while (i+z[i]+1 < len and i-z[i]-1 >= 0 and 
-               s[i+z[i]+1] == s[i-z[i]-1]) z[i] ++;
-        if (z[i] + i > z[b] + b) b = i;
+        while (i+z[i]+1 < len && i-z[i]-1 >= 0 &&
+                s[i+z[i]+1] == s[i-z[i]-1]) z[i] ++;
+        if (z[i]+i > z[b]+b) b = i;
     }
 }
