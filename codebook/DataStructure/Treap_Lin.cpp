ed771ff90909c6fbbb79f3dc1548f1872da94abd
harryoooooooooo <ymsc27884@gmail.com>
2019-02-23 16:33:46 +0800

    treap: update

--- codebook/DataStructure/Treap_Lin.cpp
+++ codebook/DataStructure/Treap_Lin.cpp
@@ -1,33 +1,24 @@
-#include <cstdio>
-#include <cstdlib>
-#include <algorithm>
-#include <string.h>
+#include <bits/stdc++.h>
 using namespace std;
-const int INF = 999999999;
+const int INF = 1<<30;
 int ran(){
-    static unsigned x = 20170928;
+    static unsigned x = 20190223;
     return x = 0xdefaced*x+1;
 }
 struct Treap{
     Treap *l,*r;
     int num,m,sz,tag,ra,ad;
     Treap(int a){
-        l=r=NULL;
-        num=m=a;
-        sz=1;
-        tag=ad=0;
+        l=r=0; num=m=a; sz=1; tag=ad=0;
         ra = ran();
     }
-}*head,*tp;
+};
 
 int size(Treap *a){
     return a ? a->sz : 0;
 }
 int min(Treap *a){
     return a ? a->m+a->ad : INF;
-}
-int add(Treap *a){
-    return a ? a->ad : 0;
 }
 void push(Treap *a){
     if(!a) return;
@@ -41,12 +32,12 @@ void push(Treap *a){
     if(a->r)a->r->ad += a->ad;
     a->num += a->ad;
     a->m += a->ad;
-    a -> ad = 0;
+    a->ad = 0;
 }
 void pull(Treap *a){
     if(!a) return;
     a->sz=1+size(a->l)+size(a->r);
-    a->m = min( a->num, min( min(a->l), min(a->r) ) );
+    a->m = min({ a->num, min(a->l), min(a->r) });
 }
 
 Treap* merge(Treap *a, Treap *b){
@@ -63,9 +54,9 @@ Treap* merge(Treap *a, Treap *b){
         return b;
     }
 }
-void split (Treap *o, Treap *&a, Treap *&b,int k){
-    if(!k) a=NULL, b=o;
-    else if(size(o)==k) a=o, b=NULL;
+void split (Treap *o, Treap *&a, Treap *&b, int k){
+    if(!k) a=0, b=o;
+    else if(size(o)==k) a=o, b=0;
     else{
         push(o);
         if(k <= size(o->l)){
@@ -80,34 +71,30 @@ void split (Treap *o, Treap *&a, Treap *&b,int k){
     }
 }
 int main(){
-    int n,tmp;
-    scanf("%d",&n);
-    for(int i = 0 ;i < n ;i++){
-        scanf("%d",&tmp);
-        tp = new Treap(tmp);
-        head = merge(head,tp);
+    Treap *head=0, *ta, *tb, *tc, *td;
+    int a, b, c, n; scanf("%d",&n);
+    for(int i=0; i<n; i++){
+        int t; scanf("%d",&t);
+        head = merge(head,new Treap(t));
     }
-    int Q;
-    scanf("%d\n",&Q);
+    int Q; scanf("%d",&Q);
     char ss[50];
-    int a, b, c;
-    Treap *ta, *tb, *tc, *td;
     while(Q--){
         scanf("%s",ss);
         if(strcmp(ss,"ADD")==0){
-            scanf("%d %d %d",&a,&b,&c);
+            scanf("%d%d%d",&a,&b,&c);
             split(head,tb,tc,b);
             split(tb,ta,tb,a-1);
             tb -> ad += c;
             head = merge(ta, merge(tb, tc));
         }else if(strcmp(ss,"REVERSE")==0){
-            scanf("%d %d",&a,&b);
+            scanf("%d%d",&a,&b);
             split(head,tb,tc,b);
             split(tb,ta,tb,a-1);
             tb -> tag ^= 1;
             head = merge(ta, merge(tb, tc));
         }else if(strcmp(ss,"REVOLVE")==0){
-            scanf("%d %d %d",&a,&b,&c);
+            scanf("%d%d%d",&a,&b,&c);
             split(head,tb,tc,b);
             split(tb,ta,tb,a-1);
             int szz = size(tb);
@@ -116,7 +103,7 @@ int main(){
             tb=merge(td,tb);
             head = merge(ta, merge(tb, tc));
         }else if(strcmp(ss,"INSERT")==0){
-            scanf("%d %d",&a,&b);
+            scanf("%d%d",&a,&b);
             split(head,ta,tc,a);
             tb = new Treap(b);
             head = merge(ta, merge(tb, tc));
@@ -127,7 +114,7 @@ int main(){
             delete tb;
             head = merge(ta,tc);
         }else if(strcmp(ss,"MIN")==0){
-            scanf("%d %d",&a,&b);
+            scanf("%d%d",&a,&b);
             split(head,tb,tc,b);
             split(tb,ta,tb,a-1);
             printf("%d\n",min(tb));
