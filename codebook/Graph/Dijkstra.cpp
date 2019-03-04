bf9ef22acf281d28463915abe47474d2c821c5a0
harryoooooooooo <ymsc27884@gmail.com>
2019-03-04 21:52:50 +0800

    update Dij

--- codebook/Graph/Dijkstra.cpp
+++ codebook/Graph/Dijkstra.cpp
@@ -1,15 +1,15 @@
-typedef struct Edge{
+struct Edge{
     int v; long long len;
-    bool operator > (const Edge &b)const { return len>b.len; }
-} State;
+    bool operator < (const Edge &b)const { return len>b.len; }
+};
 
 const long long INF = 1LL<<60;
 
 void Dijkstra(int n, vector<Edge> G[], long long d[], int s, int t=-1){
-    static priority_queue<State, vector<State>, greater<State> > pq;
+    static priority_queue<Edge> pq;
     while ( pq.size() )pq.pop();
     for (int i=1; i<=n; i++)d[i]=INF;
-    d[s]=0; pq.push( (State){s,d[s]} );
+    d[s]=0; pq.push( {s,d[s]} );
     while ( pq.size() ){
         auto x = pq.top(); pq.pop();
         int u = x.v;
@@ -18,7 +18,7 @@ void Dijkstra(int n, vector<Edge> G[], long long d[], int s, int t=-1){
         for (auto &e:G[u]){
             if (d[e.v] > d[u]+e.len){
                 d[e.v] = d[u]+e.len;
-                pq.push( (State) {e.v,d[e.v]} );
+                pq.push( {e.v,d[e.v]} );
             }
         }
     }
